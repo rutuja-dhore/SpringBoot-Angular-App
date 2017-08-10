28af6c4094dbe54f13c93b6ed54310e1470987b6
Rutuja Dhore <rutuja.dhore@neptune-int.com>
2017-08-10 23:15:04 +0530

    rest controller

--- src/main/java/com/example/demo/webapp/controller/UserController.java
+++ src/main/java/com/example/demo/webapp/controller/UserController.java
@@ -1,5 +1,32 @@
 package com.example.demo.webapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+import com.example.demo.webapp.dto.UserDTO;
+import com.example.demo.webapp.dto.UserInDTO;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public List<UserDTO> findAll() {
+		return userService.findAll();
+	}
+
+	@PostMapping
+	public UserDTO create(@RequestBody UserInDTO userInDTO) {
+		return userService.save(userInDTO);
+	}
 }
